be72ae606ec84f9ec743bbd13ead24171b743f4b
Chase Snider <jcsnider@outlook.com>
2021-03-12 12:39:25 -0500

    MVP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,64 @@
 // Write your "actions" router here!
+const express = require('express'); 
+const Actions = require('./actions-model'); 
+const mw = require('../middleware/middleware'); 
+
+const router = express.Router(); 
+
+router.get('/', (req, res, next) => {
+Actions.get()
+.then(actions => {
+  res.status(200).json(actions)
+})
+.catch(error => {
+  next(error)
+})
+});
+
+router.get('/:id', mw.actionId, (req, res) => {
+    res.status(200).json(req.action)
+});
+
+router.post('/',mw.actionId, mw.validateAction, (req, res, next) => {
+  const { project_id, description, notes } = req.body; 
+  if(!project_id || !description || !notes){
+    res.status(400).json({message:"Requires project id, description and notes"})
+  } else {
+    Actions.insert(req.body)
+    .then(action => {
+      res.status(201).json(action)
+    })
+    .catch(error => {
+      next(error);
+    })
+  }
+})
+
+router.put('/:id', mw.actionId, mw.validateAction, (req, res, next) => {
+Actions.update(req.params.id, req.body)
+.then(action => {
+  res.status(201).json(action)
+})
+.catch(error => {
+  next(error)
+})
+})
+
+router.delete('/:id', mw.actionId, (req, res, next) => {
+  Actions.remove(req.params.id)
+  .then(actions => {
+    res.status(200).json()
+  })
+  .catch(error => {
+    next(error)
+  })
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+      message: "Oh No something went wronge!",
+      error: err.message, 
+    });
+  });
+
+module.exports = router; 
--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const actionsRouter = require('./actions/actions-router'); 
+const projectsRouter = require('./projects/projects-router'); 
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+server.use(helmet());
+server.use(express.json()); 
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+res.send('<h2>Its Alive!!!!!!</h2>')
+})
+
 module.exports = server;
+
